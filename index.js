b0e3f85c963566f0c5801fbd7384088155dba3a3
alisarwarr <52290319+alisarwarr@users.noreply.github.com>
2021-01-17 01:28:57 +0500

    updated

---
+++ index.js
@@ -0,0 +1,41 @@
+const { ApolloServer, gql } = require('apollo-server');
+
+const msgs = [];
+
+const typeDefs = gql`
+    type messageType {
+        id: ID!
+        user: String!
+        content: String!
+    }
+
+    type Query {
+        messages: [messageType!]!
+    }
+
+    type Mutation {
+        postMessage(user: String!, content: String!): ID!
+    }
+`
+
+const resolvers = {
+    Query: {
+        messages: () => msgs
+    },
+    
+    Mutation: {
+        postMessage: (_, {user, content}) => {
+            let id = msgs.length;
+
+            msgs.push({ id, user, content });
+
+            return id;
+        }
+    }
+}
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+server.listen().then(({ url }) => {
+   console.log(`🚀  Server ready at ${url}`);
+})
